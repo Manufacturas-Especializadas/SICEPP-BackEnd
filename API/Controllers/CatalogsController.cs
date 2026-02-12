0cf54e0ab47b4e2bfa6b65116eecafc7d2a02e9f
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-02-12 13:58:19 -0600

    Metodos para obtener los tipos de EPP y las tallas

---
+++ API/Controllers/CatalogsController.cs
@@ -0,0 +1,37 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CatalogsController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CatalogsController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        [Route("epp-types")]
+        public async Task<IActionResult> GetEppTypes()
+        {
+            var data = await _unitOfWork.Repository<EppType>().GetAllAsync();
+
+            return Ok(data);
+        }
+
+        [HttpGet]
+        [Route("sizes")]
+        public async Task<IActionResult> GetSizes()
+        {
+            var data = await _unitOfWork.Repository<Size>().GetAllAsync();
+
+            return Ok(data);
+        }
+    }
+}
