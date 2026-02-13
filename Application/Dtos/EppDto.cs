7b6a935b63710f095494bb03045925ff3a8b3f48
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-02-13 10:10:35 -0600

    Dto Epp

---
+++ Application/Dtos/EppDto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dtos
+{
+    public class EppDto
+    {
+        public string Name { get; set; } = null!;
+
+        public string Area { get; set; } = null!;
+
+        public string Position { get; set; } = null!;
+
+        public string Shift { get; set; } = null!;
+
+        public int RequestedQuantity { get; set; }
+
+        public bool DeliveryEPPPrevious { get; set; }
+
+
+        public int EppTypeId { get; set; }
+
+        public int SizeId { get; set; }
+
+        public int ReasonRequestId { get; set; }
+
+        public int PreviousConditionId { get; set; }
+
+    }
+}
