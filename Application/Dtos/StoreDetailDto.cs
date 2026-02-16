61ef871552b65fd74f9949a551bf7b757ddafbad
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-02-16 14:48:50 -0600

    feat: store detalles

---
+++ Application/Dtos/StoreDetailDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dtos
+{
+    public class StoreDetailDto
+    {
+        public DateTime? DeliveryDate { get; set; }
+
+        public string? AuthorizedBy { get; set; }
+
+        public DateTime? LastDelivery { get; set; }
+
+        public bool? ReplacementPolicy { get; set; }
+
+        public int? StatusId { get; set; }
+
+        public bool? DeliveryConfirmation { get; set; }
+    }
+}
