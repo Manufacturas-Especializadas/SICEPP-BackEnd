33805052b47d45776ee0d4c86b68b3452d2bb5a5
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-02-16 14:43:01 -0600

    feat: endpoint para registrar un nuevo Store

---
+++ API/Controllers/StoreController.cs
@@ -0,0 +1,69 @@
+﻿using Application.Dtos;
+using Core.Entities;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StoreController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public StoreController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpPost]
+        [Route("Create")]
+        public async Task<IActionResult> CreateStore(StoreDto request)
+        {
+
+
+            var eppExists = await _unitOfWork
+                    .Repository<Epp>()
+                    .Query()
+                    .AnyAsync(x => x.Id == request.EppId);
+
+            if (!eppExists) return NotFound("EPP no encontrado");
+
+            var existingStore = await _unitOfWork
+                        .Repository<Store>()
+                        .Query()
+                        .FirstOrDefaultAsync(x => x.EppId == request.EppId);
+
+            if (existingStore == null)
+            {
+                var newStore = new Store
+                {
+                    EppId = request.EppId,
+                    DeliveryDate = request.DeliveryDate ?? DateTime.Now,
+                    AuthorizedBy = request.AuthorizedBy,
+                    LastDelivery = request.LastDelivery,
+                    ReplacementPolicy = request.ReplacementPolicy ?? false,
+                    StatusId = request.StatusId,
+                    DeliveryConfirmation = request.DeliveryConfirmation ?? false
+                };
+
+                await _unitOfWork.Repository<Store>().AddAsync(newStore);
+            }
+            else
+            {
+                existingStore.DeliveryDate = request.DeliveryDate;
+                existingStore.AuthorizedBy = request.AuthorizedBy;
+                existingStore.LastDelivery = request.LastDelivery;
+                existingStore.ReplacementPolicy = request.ReplacementPolicy!.Value;
+                existingStore.StatusId = request.StatusId;
+                existingStore.DeliveryConfirmation = request.DeliveryConfirmation!.Value;
+            }
+
+            await _unitOfWork.SaveAsync();
+
+            return Ok(new { message = "Store guardado correctamente" });
+        }
+    }
+}
