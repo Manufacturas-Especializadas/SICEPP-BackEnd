f3a9bb3d1645ec0d0cfb948c81a40ba8362f5e37
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-02-16 14:48:37 -0600

    feat: DTO para store

---
+++ Application/Dtos/StoreDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dtos
+{
+    public class StoreDto
+    {
+        public int EppId {  get; set; }
+
+        public DateTime? DeliveryDate { get; set; }
+
+        public string? AuthorizedBy { get; set; }
+
+        public DateTime? LastDelivery {  get; set; }
+
+        public bool? ReplacementPolicy { get; set; }
+
+        public int StatusId { get; set; }
+
+        public bool? DeliveryConfirmation { get; set; }
+    }
+}
