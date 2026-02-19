f15101abda1705a013e6878dcbbb9a37bd72d63f
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-02-19 07:37:10 -0600

    feat: epp dto detalles

---
+++ Application/Dtos/EppDetailDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dtos
+{
+    public class EppDetailDto
+    {
+        public int EppTypeId { get; set; }
+
+        public int? SizeId { get; set; }
+
+        public int RequestedQuantity { get; set; }
+    }
+}
