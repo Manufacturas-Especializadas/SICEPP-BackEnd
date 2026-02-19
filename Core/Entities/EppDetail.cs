602af446260f06506b5b8b349d923686e9726972
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-02-19 07:40:10 -0600

    feat: modelo epp detalles

---
+++ Core/Entities/EppDetail.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class EppDetail
+    {
+        public int Id { get; set; }
+
+        public int EppId { get; set; }
+
+        public int EppTypeId { get; set; }
+
+        public int? SizeId { get; set; }
+
+        public int RequestedQuantity { get; set; }
+
+        public virtual Epp Epp { get; set; } = null!;
+
+        public virtual EppType EppType { get; set; } = null!;
+
+        public virtual Size? Size { get; set; }
+    }
+}
