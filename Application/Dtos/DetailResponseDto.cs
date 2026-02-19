bff10f5abe1cf6f817525169bc23d96e024258bc
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-02-19 07:36:51 -0600

    feat: dto detalles

---
+++ Application/Dtos/DetailResponseDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dtos
+{
+    public class DetailResponseDto
+    {
+        public string EppType { get; set; }
+
+        public string Size { get; set; }
+
+        public int RequestedQuantity { get; set; }
+    }
+}
